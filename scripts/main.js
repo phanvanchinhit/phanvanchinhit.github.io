473ffc9a63f4441d323f191be398be6f3f803c18
phanvanchinhit <danhchinh20@gmail.com>
2021-04-18 02:32:32 +0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,8 @@ AOS.init({
   duration: 1000
 });
 
-// Add your javascript here
+$(document).ready(function(){
+    $(".menu-toggle a").click(function(){
+        $(".menu").slideToggle(700);
+    });
+});
